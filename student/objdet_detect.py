381b66e311b36ad304ae5149735ab1a1412357f6
PowerYingMu <xwang15@mtu.edu>
2022-09-24 15:25:11 -0400

    3D Object Detection Step 3 and 4

--- student/objdet_detect.py
+++ student/objdet_detect.py
@@ -28,7 +28,7 @@ from tools.objdet_models.resnet.utils.evaluation_utils import decode, post_proce
 
 from tools.objdet_models.darknet.models.darknet2pytorch import Darknet as darknet
 from tools.objdet_models.darknet.utils.evaluation_utils import post_processing_v2
-
+from tools.objdet_models.resnet.utils.torch_utils import _sigmoid
 
 # load model-related parameters into an edict
 def load_configs_model(model_name='darknet', configs=None):
@@ -61,6 +61,40 @@ def load_configs_model(model_name='darknet', configs=None):
         ####### ID_S3_EX1-3 START #######     
         #######
         print("student task ID_S3_EX1-3")
+        configs.arch = 'fpn_resnet_18'
+        configs.saved_fn = 'fpn_resnet_18'
+        configs.model_path = configs.model_path = os.path.join(parent_path, 'tools', 'objdet_models', 'resnet')
+        configs.pretrained_filename = os.path.join(configs.model_path, 'pretrained', 'fpn_resnet_18_epoch_300.pth')
+        configs.K = 50
+        configs.gpu_idx = 0
+        configs.num_workers = 1
+        configs.batch_size = 1
+        configs.conf_thresh = 0.5
+        configs.peak_thresh = 0.2
+        configs.otuput_format = 'image'
+        configs.output_video_fn = 'out_fpn_resnet_18'
+        configs.distributed = False
+        configs.input_size = (608, 608)
+        configs.hm_size = (152, 152)
+        configs.down_ratio = 4
+        configs.max_objects = 50
+
+        configs.imagenet_pretrained = False
+        configs.head_conv = 64
+        configs.num_classes = 3
+        configs.num_center_offset = 2
+        configs.num_z = 1
+        configs.num_dim = 3
+        configs.num_direction = 2  # sin, cos
+
+        configs.heads = {
+            'hm_cen': configs.num_classes,
+            'cen_offset': configs.num_center_offset,
+            'direction': configs.num_direction,
+            'z_coor': configs.num_z,
+            'dim': configs.num_dim
+        }
+        configs.num_input_features = 4
 
         #######
         ####### ID_S3_EX1-3 END #######     
@@ -118,6 +152,7 @@ def create_model(configs):
         ####### ID_S3_EX1-4 START #######     
         #######
         print("student task ID_S3_EX1-4")
+        model=fpn_resnet.get_pose_net(18,configs.heads,configs.head_conv,configs.imagenet_pretrained)
 
         #######
         ####### ID_S3_EX1-4 END #######     
@@ -168,6 +203,13 @@ def detect_objects(input_bev_maps, model, configs):
             #######
             print("student task ID_S3_EX1-5")
 
+            detections = decode(outputs['hm_cen'], outputs['cen_offset'], outputs['direction'], outputs['z_coor'],outputs['dim'], K=configs.k)
+            detections = detections.cpu().numpy().astype(np.float32)
+            detections = post_processing(detections, configs)
+            detections = detections[0][1]
+
+            print(detections)
+
             #######
             ####### ID_S3_EX1-5 END #######     
 
@@ -180,10 +222,22 @@ def detect_objects(input_bev_maps, model, configs):
     objects = [] 
 
     ## step 1 : check whether there are any detections
+    if len(detections) > 0:
 
         ## step 2 : loop over all detections
-        
+        for det in detections:
+            id_, bev_x, bev_y, z, h, bev_w, bev_l, yaw = det
             ## step 3 : perform the conversion using the limits for x, y and z set in the configs structure
+            x = bev_y / configs.bev_height * (configs.lim_x[1] - configs.lim_x[0])
+            y = bev_x / configs.bev_width * (configs.lim_y[1] - configs.lim_y[0]) - (configs.lim_y[1] - configs.lim_y[0])/2.0
+            w = bev_w / configs.bev_width * (configs.lim_y[1] - configs.lim_y[0])
+            l = bev_l / configs.bev_height * (configs.lim_x[1] - configs.lim_x[0])
+            ## step 3 : perform the conversion using the limits for x, y and z set in the configs structure
+            if ((x >= configs.lim_x[0]) and (x <= configs.lim_x[1]) 
+                and (y >= configs.lim_y[0]) and (y <= configs.lim_y[1])
+                and (z >= configs.lim_z[0]) and (z <= configs.lim_z[1])):
+                
+                objects.append([1, x, y, z, h, w, l, yaw])
         
             ## step 4 : append the current object to the 'objects' array
         
